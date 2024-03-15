7c73d887d95c626775c87251cc91fa2c60ba1dbf
webdev-bfunk <webdev.bfunk@gmail.com>
2024-03-15 13:09:29 -0400

    replaced apostraphes

--- app/component/Features.jsx
+++ app/component/Features.jsx
@@ -8,7 +8,7 @@ const Features = () => {
         {/* Whats Different */}
         <div className="flex flex-col space-y-12 md:w-1/2">
           <h2 className="max-w-md text-4xl font-bold text-center md:text-left">
-            What's different about Manage?
+            What&apos;s different about Manage?
           </h2>
           <p className="max-w-sm text-center text-darkGrayishBlue md:text-left">
             Manage provides all the functionality your team needs, without the
--- app/component/Testimonials.jsx
+++ app/component/Testimonials.jsx
@@ -6,7 +6,7 @@ const Testimonials = () => {
     <section id="testimonials" className="mb-20">
       <div className="max-w-6xl px-5 mx-auto mt-32 text-center">
         <h2 className="text-4xl font-bold text-center">
-          What's Different About Manage?
+          What&apos;s Different About Manage?
         </h2>
         {/* Testimonials Container */}
         <div className="flex flex-col mt-24 md:flex-row md:space-x-6">
